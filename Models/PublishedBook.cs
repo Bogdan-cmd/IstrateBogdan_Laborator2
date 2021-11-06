e355e7df725393596f4b79c99ebc3df3d191ffb0
Bogdan-cmd <bogdan.d.istrate@gmail.com>
2021-11-06 14:19:20 +0200

    Add project files.

---
+++ Models/PublishedBook.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Policy;
+
+namespace IstrateBogdan_Laborator2.Models
+{
+    public class PublishedBook
+    {
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public Publisher Publisher { get; set; }
+        public Book Book { get; set; }
+
+    }
+}
